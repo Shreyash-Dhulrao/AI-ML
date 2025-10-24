5e6f7215e50c2a76dbc6767f3f17da7431a7707f
Shreyash Dhulrao <shreyashdhularao@gmail.com>
2025-10-24 16:03:16 +0530

    Functions and its arguments

--- Learning/Python/01_program.py
+++ Learning/Python/01_program.py
@@ -1,2 +1,13 @@
-print("Hello World")
-print("From 24-10-2025 We are going to learn about ai ml using python")
+# Functions and its arguments
+
+def args(x , y , c=5): # Default argument in c = 5
+    d = ( x + y + c )/3
+    # print(d) # This will print the values but we cant save it in any variable
+    return d
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(f"The value of a and b is: {args(a , b)}")
+print(f"The value of a and b with new c value is: {args(a , b, 20)}") # Overriding default argument
+# args(y = 30, x = 10) # Keyword arguments
