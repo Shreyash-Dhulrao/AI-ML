cc5fdd284332d2c714e1e19669bc5d6f50aac057
Shreyash Dhulrao <shreyashdhularao@gmail.com>
2025-10-25 11:08:23 +0530

    Scopes in variables-Local and updating the gloabl variable using function

--- Learning/Python/25-10-2025/02_Scope.py
+++ Learning/Python/25-10-2025/02_Scope.py
@@ -14,4 +14,22 @@ c = 5 # This is a global variable
 print(funct(5, 10))
 '''
 
+def greet():
+    c = 10 # This is local variable
+    return c
 
+''' 
+print(c) # This will throw an error saying c is not defined
+if we want to use the c outside the function then we have to use the global keyword before declaring the vairable
+'''
+
+ca = 5
+def greet2():
+    global ca
+    ca = 20
+    return ca
+
+print(greet2()) # From this the value of ca will override on existing value
+print(ca)
+
+# Note: The global variable will not work until there is previously value of that variable like in above example we declare the ca first then in function greet2 we have modified it using global keyword
