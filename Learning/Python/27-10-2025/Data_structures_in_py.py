3ece49fd321229faed7b509d2c7da2349927da33
Shreyash Dhulrao <shreyashdhularao@gmail.com>
2025-10-27 14:45:51 +0530

    Data structures

---
+++ Learning/Python/27-10-2025/Data_structures_in_py.py
@@ -0,0 +1,7 @@
+'''
+There are Total 4 types of data structure to store the data:
+1. Lists: mutable(manipulate the data in any form)
+2. Tuples: immutable
+3. Sets: stores the data in random order
+4. Dictionary: stores data in key value pair
+'''
