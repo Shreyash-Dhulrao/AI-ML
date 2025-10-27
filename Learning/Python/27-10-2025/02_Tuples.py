9bfedefc17507077dcfa4af1d7fe4541486f1593
Shreyash Dhulrao <shreyashdhularao@gmail.com>
2025-10-27 14:53:35 +0530

    DS: Tuples

---
+++ Learning/Python/27-10-2025/02_Tuples.py
@@ -0,0 +1,5 @@
+tup = (23, 22.2 , "red")
+
+print(tup)
+print(tup.count(23)) # This use to count how much elements are present in the tuple of which we are giving the value
+print(tup.index("red")) # This will print the index value of the element
