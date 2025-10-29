818d7f3e3fad0baf09bffaf2fd35aa5e19e030ef
Shreyash Dhulrao <shreyashdhularao@gmail.com>
2025-10-29 13:15:12 +0530

    OOPS Concepts

---
+++ Learning/Python/29-10-2025/01_OOPs.py
@@ -0,0 +1,9 @@
+''' OOPs is known for object oriented programming where we learn about 
+1. Inheritance
+2. Encapsulation
+3. Abstraction
+4. Polymorphism
+
+In oops we majorly learn about classes and objects
+
+'''
